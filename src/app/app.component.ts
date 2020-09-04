2439cef02c5e9db46b9ec3d3fec7aca36d6b2f8a
tholakelebusi <tholakelebusi@gmail.com>
2020-09-04 03:47:50 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import {enableProdMode} from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -7,5 +8,5 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
-  enableProdMode();
+ 
 }
