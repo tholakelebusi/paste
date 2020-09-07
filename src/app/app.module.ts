9e41cad8037bbe612cb071bb5a0519674e7a1ae7
tholakelebusi <tholakelebusi@gmail.com>
2020-09-07 12:54:41 +0100

    1 file modified, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
 import {MatDialog} from '@angular/material/dialog';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent,],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
